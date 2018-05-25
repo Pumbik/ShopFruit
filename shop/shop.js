1183b63958e0a5dd22558435947055363f78163e
Pumbik <lepiks@ukr.net>
2018-05-25 14:19:11 +0300

    Test Store, based on google docs. On JQuery.

---
+++ shop/shop.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $.getJSON("https://spreadsheets.google.com/feeds/list/18aIuAMG8EAW1yWqB8qeu9TaVl0bYSHwJx4jaHHjaEOk/od6/public/values?alt=json",
+        function(data) {
+            data = data['feed']['entry'];
+            console.log(data);
+            showGoods(data);
+        });
+
+    function showGoods(data) {
+        var out = '';
+
+        for (var i = 0; i < data.length; i++) {
+            if (data[i]['gsx$show']['$t'] != 0) {
+                out += `<div class="col-lg-3 col-md col-sm-2 test-center">`;
+                out += `<div class="goods">`;
+                out += `<h5>${data[i]['gsx$name']['$t']}</h5>`;
+                out += `<img src="${data[i]['gsx$image']['$t']}" alt="fruit">`;
+                out += `<p class="cost">Цена: ${data[i]['gsx$cost']['$t']}</p>`;
+                out += `</div>`;
+                out += `</div>`;
+            }
+
+        }
+
+
+
+        $('.shop-field').html(out);
+    }
+})
